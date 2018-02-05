cfeb4e1c16331f797b81a2b92b4ee2a0de35c85e
Vidya.Gowda <Vidya.Gowda@ICPL11548.infostretch.com>
2018-02-05 15:21:44 +0530

    upd test

--- src/main/java/src/main/java/BaseTest.java
+++ src/main/java/src/main/java/BaseTest.java
@@ -3,29 +3,32 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 	
 	public static WebDriver driver;
-	
+	@Parameters("browser")
 	@BeforeMethod
-	public void precondtion()
+	public void precondtion(@Optional("firefox")String browsertype)
 	
 	{ 
-		/*if(browsertype.equalsIgnoreCase("chrome"))
-		{*/
+		if(browsertype.equalsIgnoreCase("chrome"))
+		{
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		driver=new ChromeDriver();
 		
-		/*}
+		}
 		
 		else if(browsertype.equalsIgnoreCase("firefox"))
 		{
 			System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
 			driver=new FirefoxDriver();
-		}*/
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.get("https://www.orbitz.com");
